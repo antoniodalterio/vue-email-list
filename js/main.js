a679b6613bc2239611b49499496831dbe7ba0a6e
antoniodalterio <antoniodalterio2227@gmail.com>
2023-12-05 19:58:26 +0100

    feat: generazione email tramite API

--- js/main.js
+++ js/main.js
@@ -3,6 +3,20 @@ const { createApp } = Vue;
 
 createApp({
   data() {
-    return {};
+    return {
+      emails: [],
+    };
+  },
+  methods: {},
+  mounted() {
+    for (let i = 0; i < 10; i++) {
+      axios
+        .get('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then((response) => {
+          this.emails = response.data.response;
+          console.log(this.emails);
+          console.log(this.emails);
+        });
+    }
   },
 }).mount('#app');
